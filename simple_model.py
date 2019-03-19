8e4b79c03047b044ea991548c812d666c499e707
Emre Arda Genc <emregenc2019@u.northwestern.edu>
2019-03-18 20:42:26 -0500

    removed unnecessary parts from  simple_model.py

--- simple_model.py
+++ simple_model.py
@@ -10,6 +10,7 @@ from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint
 import numpy as np
 import argparse
+from data_processing import process_data
 
 """To run this code, you'll need to first download and extract the text dataset
     from here: http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz. Change the
@@ -25,49 +26,54 @@ args = parser.parse_args()
 if args.data_path:
     data_path = args.data_path
 
-def read_words(filename):
-    with tf.gfile.GFile(filename, "r") as f:
-        return f.read().replace("\n", "<eos>").split()
+
+# def read_words(filename):
+'''reads text from file and returns an array of words '''
+#     with tf.gfile.GFile(filename, "r") as f:
+#         return f.read().replace("\n", "<eos>").split()
 
 
-def build_vocab(filename):
-    data = read_words(filename)
+# def build_vocab(filename):
+'''sorts words in decreasing frequency, returns a dictionary that assigns each word to a unique id based on how often it is used '''
+#     data = read_words(filename)
 
-    counter = collections.Counter(data)
-    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+#     counter = collections.Counter(data)
+#     count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
 
-    words, _ = list(zip(*count_pairs))
-    word_to_id = dict(zip(words, range(len(words))))
+#     words, _ = list(zip(*count_pairs))
+#     word_to_id = dict(zip(words, range(len(words))))
 
-    return word_to_id
-
-
-def file_to_word_ids(filename, word_to_id):
-    data = read_words(filename)
-    return [word_to_id[word] for word in data if word in word_to_id]
+#     return word_to_id
 
 
-def load_data():
-    # get the data paths
-    train_path = os.path.join(data_path, "ptb.train.txt")
-    valid_path = os.path.join(data_path, "ptb.valid.txt")
-    test_path = os.path.join(data_path, "ptb.test.txt")
+# def file_to_word_ids(filename, word_to_id):
+'''returns a list of word ids for each word that is in our vocabulary list'''
+#     data = read_words(filename)
+#     return [word_to_id[word] for word in data if word in word_to_id]
 
-    # build the complete vocabulary, then convert text data to list of integers
-    word_to_id = build_vocab(train_path)
-    train_data = file_to_word_ids(train_path, word_to_id)
-    valid_data = file_to_word_ids(valid_path, word_to_id)
-    test_data = file_to_word_ids(test_path, word_to_id)
-    vocabulary = len(word_to_id)
-    reversed_dictionary = dict(zip(word_to_id.values(), word_to_id.keys()))
 
-    print(train_data[:5])
-    print(word_to_id)
-    print(vocabulary)
-    print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
-    return train_data, valid_data, test_data, vocabulary, reversed_dictionary
+# def load_data():
+''' loads training, testing, and validation data, creates a reverse dictionary that maps word id s to words'''
+#     # get the data paths
+#     train_path = os.path.join(data_path, "ptb.train.txt")
+#     valid_path = os.path.join(data_path, "ptb.valid.txt")
+#     test_path = os.path.join(data_path, "ptb.test.txt")
 
-train_data, valid_data, test_data, vocabulary, reversed_dictionary = load_data()
+#     # build the complete vocabulary, then convert text data to list of integers
+#     word_to_id = build_vocab(train_path)
+#     train_data = file_to_word_ids(train_path, word_to_id)
+#     valid_data = file_to_word_ids(valid_path, word_to_id)
+#     test_data = file_to_word_ids(test_path, word_to_id)
+#     vocabulary = len(word_to_id)
+#     reversed_dictionary = dict(zip(word_to_id.values(), word_to_id.keys()))
+
+#     print(train_data[:5])
+#     print(word_to_id)
+#     print(vocabulary)
+#     print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
+#     return train_data, valid_data, test_data, vocabulary, reversed_dictionary
+
+# train_data, valid_data, test_data, vocabulary, reversed_dictionary = load_data()
 
 
 class KerasBatchGenerator(object):

364106c86f530b69567c73f8f49f83ff451bb730
Emre Arda Genc <emregenc2019@u.northwestern.edu>
2019-03-18 23:25:49 -0500

    modified simple model to accommodate notes instead of words

--- simple_model.py
+++ simple_model.py
@@ -47,7 +47,7 @@ if args.data_path:
 
 
 # def file_to_word_ids(filename, word_to_id):
-'''returns a list of word ids for each word that is in our vocabulary list'''
+'''returns a list of word ids for each word that is in our num_notes list'''
 #     data = read_words(filename)
 #     return [word_to_id[word] for word in data if word in word_to_id]
 
@@ -59,30 +59,35 @@ if args.data_path:
 #     valid_path = os.path.join(data_path, "ptb.valid.txt")
 #     test_path = os.path.join(data_path, "ptb.test.txt")
 
-#     # build the complete vocabulary, then convert text data to list of integers
+#     # build the complete num_notes, then convert text data to list of integers
 #     word_to_id = build_vocab(train_path)
 #     train_data = file_to_word_ids(train_path, word_to_id)
 #     valid_data = file_to_word_ids(valid_path, word_to_id)
 #     test_data = file_to_word_ids(test_path, word_to_id)
-#     vocabulary = len(word_to_id)
+#     num_notes = len(word_to_id)
 #     reversed_dictionary = dict(zip(word_to_id.values(), word_to_id.keys()))
 
 #     print(train_data[:5])
 #     print(word_to_id)
-#     print(vocabulary)
+#     print(num_notes)
 #     print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
-#     return train_data, valid_data, test_data, vocabulary, reversed_dictionary
+#     return train_data, valid_data, test_data, num_notes, reversed_dictionary
 
-# train_data, valid_data, test_data, vocabulary, reversed_dictionary = load_data()
+# train_data, valid_data, test_data, num_notes, reversed_dictionary = load_data()
 
+train_data, test_data, valid_data, num_notes = process_data()
 
 class KerasBatchGenerator(object):
 
-    def __init__(self, data, num_steps, batch_size, vocabulary, skip_step=5):
+    def __init__(self, data, num_steps, batch_size, num_notes, skip_step=5):
+        #train, test, or valid data, output of process_data()
         self.data = data
+        #number of notes model will learn from, number of notes we will input at each step
         self.num_steps = num_steps
+        #how many notes will each batch consist of? make each song a batch?
         self.batch_size = batch_size
-        self.vocabulary = vocabulary
+        #number of unique notes used
+        self.num_notes = num_notes
         # this will track the progress of the batches sequentially through the
         # data set - once the data reaches the end of the data set it will reset
         # back to zero
@@ -93,7 +98,7 @@ class KerasBatchGenerator(object):
 
     def generate(self):
         x = np.zeros((self.batch_size, self.num_steps))
-        y = np.zeros((self.batch_size, self.num_steps, self.vocabulary))
+        y = np.zeros((self.batch_size, self.num_steps, self.num_notes))
         while True:
             for i in range(self.batch_size):
                 if self.current_idx + self.num_steps >= len(self.data):
@@ -102,26 +107,26 @@ class KerasBatchGenerator(object):
                 x[i, :] = self.data[self.current_idx:self.current_idx + self.num_steps]
                 temp_y = self.data[self.current_idx + 1:self.current_idx + self.num_steps + 1]
                 # convert all of temp_y into a one hot representation
-                y[i, :, :] = to_categorical(temp_y, num_classes=self.vocabulary)
+                y[i, :, :] = to_categorical(temp_y, num_classes=self.num_notes)
                 self.current_idx += self.skip_step
             yield x, y
 
 num_steps = 30
 batch_size = 20
-train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, vocabulary,
+train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, num_notes,
                                            skip_step=num_steps)
-valid_data_generator = KerasBatchGenerator(valid_data, num_steps, batch_size, vocabulary,
+valid_data_generator = KerasBatchGenerator(valid_data, num_steps, batch_size, num_notes,
                                            skip_step=num_steps)
 
-hidden_size = 500
+hidden_size = num_notes
 use_dropout=True
 model = Sequential()
-model.add(Embedding(vocabulary, hidden_size, input_length=num_steps))
+model.add(Embedding(num_notes, hidden_size, input_length=num_steps))
 model.add(LSTM(hidden_size, return_sequences=True))
 model.add(LSTM(hidden_size, return_sequences=True))
 if use_dropout:
     model.add(Dropout(0.5))
-model.add(TimeDistributed(Dense(vocabulary)))
+model.add(TimeDistributed(Dense(num_notes)))
 model.add(Activation('softmax'))
 
 optimizer = Adam()
@@ -134,14 +139,11 @@ if args.run_opt == 1:
     model.fit_generator(train_data_generator.generate(), len(train_data)//(batch_size*num_steps), num_epochs,
                         validation_data=valid_data_generator.generate(),
                         validation_steps=len(valid_data)//(batch_size*num_steps), callbacks=[checkpointer])
-    # model.fit_generator(train_data_generator.generate(), 2000, num_epochs,
-    #                     validation_data=valid_data_generator.generate(),
-    #                     validation_steps=10)
     model.save(data_path + "final_model.hdf5")
 elif args.run_opt == 2:
     model = load_model(data_path + "/model-40.hdf5")
     dummy_iters = 40
-    example_training_generator = KerasBatchGenerator(train_data, num_steps, 1, vocabulary,
+    example_training_generator = KerasBatchGenerator(train_data, num_steps, 1, num_notes,
                                                      skip_step=1)
     print("Training data:")
     for i in range(dummy_iters):
@@ -159,7 +161,7 @@ elif args.run_opt == 2:
     print(pred_print_out)
     # test data set
     dummy_iters = 40
-    example_test_generator = KerasBatchGenerator(test_data, num_steps, 1, vocabulary,
+    example_test_generator = KerasBatchGenerator(test_data, num_steps, 1, num_notes,
                                                      skip_step=1)
     print("Test data:")
     for i in range(dummy_iters):
